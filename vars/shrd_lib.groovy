a86bd8d550008f7601087828c1d714b7f6875382
trainyou <kittu9.nine@gmail.com>
2024-03-18 15:39:13 +0530

    slack notifications shared library added

---
+++ vars/shrd_lib.groovy
@@ -0,0 +1,10 @@
+def slack_not(){
+  post {
+    success {
+      slackSend channel:'#jenkins-for-now', color: '#71E600', message: "$env.JOB_NAME -->$env.BUILD_NUMBER"
+    }
+    failure {
+      slackSend channel:'#jenkins-for-now', color: '#BF3A00', message: "$env.JOB_NAME --> $env.BUILD_NUMBER"
+    }
+  }
+}
