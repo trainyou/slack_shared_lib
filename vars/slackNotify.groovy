244d1f98246cf7f7cd5b0b52c4930e1b4be766db
trainyou <kittu9.nine@gmail.com>
2024-03-19 11:57:23 +0530

    function corrected

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,10 +1,12 @@
 def call(){
-  post {
-    success {
-      slackSend channel:'#jenkins-for-now', color: '#71E600', message: "$env.JOB_NAME -->$env.BUILD_NUMBER"
-    }
-    failure {
-      slackSend channel:'#jenkins-for-now', color: '#BF3A00', message: "$env.JOB_NAME --> $env.BUILD_NUMBER"
+  script {//This is to encapsulate declarative in groovy function
+    post {
+      success {
+        slackSend channel:'#jenkins-for-now', color: '#71E600', message: "$env.JOB_NAME -->$env.BUILD_NUMBER"
+      }
+      failure {
+        slackSend channel:'#jenkins-for-now', color: '#BF3A00', message: "$env.JOB_NAME --> $env.BUILD_NUMBER"
+      }
     }
   }
 }
