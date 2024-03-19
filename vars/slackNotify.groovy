92cce2ee79b1d9f8d68df163feb8c66df8cb29f3
trainyou <kittu9.nine@gmail.com>
2024-03-19 11:25:47 +0530

    function is modified as the post is not included in the function

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,8 +1,10 @@
 def call(){
-  success {
-    slackSend channel:'#jenkins-for-now', color: '#71E600', message: "$env.JOB_NAME -->$env.BUILD_NUMBER"
-  }
-  failure {
-    slackSend channel:'#jenkins-for-now', color: '#BF3A00', message: "$env.JOB_NAME --> $env.BUILD_NUMBER"
+  post {
+    success {
+      slackSend channel:'#jenkins-for-now', color: '#71E600', message: "$env.JOB_NAME -->$env.BUILD_NUMBER"
+    }
+    failure {
+      slackSend channel:'#jenkins-for-now', color: '#BF3A00', message: "$env.JOB_NAME --> $env.BUILD_NUMBER"
+    }
   }
 }
