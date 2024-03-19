067415a2809c0c59edca40edea44159443041284
trainyou <kittu9.nine@gmail.com>
2024-03-19 13:02:10 +0530

    modified

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,5 +1,6 @@
 def call(){
-  script {//This is to encapsulate declarative in groovy function
+  pipeline {
+    //script {This is to encapsulate declarative in groovy function
     post {
       success {
         slackSend channel:'#jenkins-for-now', color: '#71E600', message: "$env.JOB_NAME -->$env.BUILD_NUMBER"
@@ -8,5 +9,6 @@ def call(){
         slackSend channel:'#jenkins-for-now', color: '#BF3A00', message: "$env.JOB_NAME --> $env.BUILD_NUMBER"
       }
     }
+    //}
   }
 }
